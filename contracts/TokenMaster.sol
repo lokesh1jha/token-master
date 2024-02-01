145154787aab88eb836be6635bbd7e2472f98e38
lokeshjha <lokesh1jha@gmai.com>
2024-02-02 00:06:00 +0530

    occasions added by list function

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -4,10 +4,48 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
+    address public owner;
+    uint256 public totalOcassions;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) occasions;
+
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {
-        
+        owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTicket,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public {
+        totalOcassions++;
+
+        occasions[totalOcassions] = Occasion(
+            totalOcassions,
+            _name,
+            _cost,
+            _maxTicket,
+            _maxTicket,
+            _date,
+            _time,
+            _location
+        );
     }
 }
